f9a40226a8f2ba32de0b32dbf8212c5b721cf343
Jonatthan Medalla <jonatthan.medalla@inacapmail.cl>
2024-12-23 02:14:48 -0300

    ok

---
+++ humanizador/src/Titulo.jsx
@@ -0,0 +1,10 @@
+
+function App(){
+    const nombre = 'Jonathan';
+      if(nombre){
+        return <h1>Hola {nombre}</h1>
+      }
+    return <h1>Hola Desconocido</h1>
+  }
+  
+export default Titulo; 
